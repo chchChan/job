2189c6e79736940643196282ee73e1eb3fb54762
KwonE <117220481+chchChan@users.noreply.github.com>
2025-06-25 03:55:51 +0900

    지도 + 헤더푸터 수정

---
+++ src/main/resources/static/public/js/main/kakaoMap.js
@@ -0,0 +1,34 @@
+function initMapWithAddress(address) {
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    geocoder.addressSearch(address, function (result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 1. 지도 생성
+            const mapContainer = document.getElementById('map');
+            const mapOption = {
+                center: coords,
+                level: 3
+            };
+            const map = new kakao.maps.Map(mapContainer, mapOption);
+
+            // 2. 마커 생성
+            const marker = new kakao.maps.Marker({
+                position: coords
+            });
+            marker.setMap(map);
+        } else {
+            console.error("주소 변환 실패:", status);
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const addressDiv = document.getElementById('addressDiv');
+    const address = addressDiv.innerText.trim();
+
+    if (address) {
+        initMapWithAddress(address);
+    }
+});
